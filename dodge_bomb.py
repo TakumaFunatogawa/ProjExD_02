dd4b5ad8f67f78520742ddbfab806ea51b7fc735
c0b20138 <c0b20138a2@edu.teu.ac.jp>
2023-04-25 14:12:02 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import sys
 
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -8,16 +9,20 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img = pg.Surface((20, 20))  # 練習1
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習1
+    bb_img.set_colorkey((0, 0, 0))  # 練習1
     tmr = 0
 
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return 0
 
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img, [600, 200])
 
         pg.display.update()
         clock.tick(1000)
