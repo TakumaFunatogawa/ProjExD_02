97b71dd6124ccb592f10eacdae939c4ac667d2ef
c0b20138 <c0b20138a2@edu.teu.ac.jp>
2023-04-25 14:18:54 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
-import pygame as pg
+import random
 import sys
+import pygame as pg
 
 
 def main():
@@ -12,6 +13,8 @@ def main():
     bb_img = pg.Surface((20, 20))  # 練習1
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習1
     bb_img.set_colorkey((0, 0, 0))  # 練習1
+    x, y = random.randint(0, 1600), random.randint(0, 900)  # 練習2
+    screen.blit(bb_img, [x, y])  # 練習2
     tmr = 0
 
     while True:
@@ -22,7 +25,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [600, 200])
+        screen.blit(bb_img, [x, y])
 
         pg.display.update()
         clock.tick(1000)
